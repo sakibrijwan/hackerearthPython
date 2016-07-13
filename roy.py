5aefeccaf8b81be73736297ddad6ae81442d2c2e
Sakib <atiquereza@yahoo.com>
2016-07-13 16:37:43 +0600

    done

--- roy.py
+++ roy.py
@@ -4,35 +4,47 @@ N = int(input())
 W = [None] * N
 H = [None] * N
 x = [None] * N
+y=[None]*N
 result = [None] * N
 check=N
+inp=N
 
-while check>0:
-    x = input().split()
-    check-=1
-    print(x)
-len=N
-while len>0:
-        W[i] = int(x[i])
-        H[i] = int(x[i+1])
-        i+=1
-        len-=1
+x= input().split()
+
+while inp-1>0:
+    y=input().split()
+    for w in y:
+        x.append(w)
+
+    inp-=1
+
+lenth=len(x)
+W=x[1::2]
+H=x[::2]
+
+
+res=N
+
 j=0
 while N > 0:
 
         if N != 0 or N < 1000:
-             if l > W[j] or l > H[j]:
+             if l > int(W[j]) or l > int(H[j]):
                 result[j] = "UPLOAD ANOTHER"
 
-             if l <= W[j] and l <= H[j] and W[j] == H[j]:
+             if l <= int(W[j]) and l <= int(H[j]) and W[j] == H[j]:
                 result[j] = "ACCEPTED"
 
-             if l <= W[j] and l <= H[j] and W[j] != H[j]:
+             if l <= int(W[j]) and l <= int(H[j]) and W[j] != H[j]:
                 result[j] = "CROP IT"
              j+=1
 
         N -= 1
-print(result)
+index=0
+for name in result:
+    print(result[index])
+    index += 1
+
 
 
 
