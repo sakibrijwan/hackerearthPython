5239cfb7a030c52bbf0f99dd7fbc0232aae28288
Sakib <atiquereza@yahoo.com>
2016-07-14 12:58:44 +0600

    complete

--- countDivisor.py
+++ countDivisor.py
@@ -1,9 +1,19 @@
 inp= [None]*10
-
 inp=input().split()
-print(inp)
 len=len(inp)
 result=[0]*len
-print(range(10))
-print(range(int(inp[0]),int(inp[1])))
-print(result)
+
+i=0
+v1=int(inp[0])
+v2=int(inp[1])
+v3=int(inp[2])
+if v1==v2:
+    i=0
+else:
+
+    while v1<=v2:
+        if v1%v3==0:
+          i+=1
+        v1+=1
+
+print(i)
